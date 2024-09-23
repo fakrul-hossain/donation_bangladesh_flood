a88bdfed043cc1163abe94777a95aabe83aabcb0
fakrul-hossain <fakrulhossain912@gmail.com>
2024-09-23 15:12:55 +0600

    navbar complete

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+      extend: {
+        fontFamily: {
+          custom: ['"Lexend", sans-serif'],
+        },
+        colors: {
+          primaryColor: 'rgb(180, 244, 97);',
+          customGreen: '#17BF63',
+        },
+      },
+    },
+    plugins: [],
+  }
